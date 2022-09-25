c43c8a4b99328b157a51aa88894d33ebaebb47a7
Nesae <92125893+Nesae-avi@users.noreply.github.com>
2022-09-25 18:03:52 +0200

    Fix name convention violation

--- LaunchFoundationGameCamera/Components/Logger.cs
+++ LaunchFoundationGameCamera/Components/Logger.cs
@@ -5,24 +5,24 @@ namespace LaunchFoundationGameCamera.Components
     // Very simple logging class.
     internal class Logger
     {
-        private static string assembly_filename => $"{Assembly.GetExecutingAssembly().GetName().Name}";
-        private static string filename => assembly_filename == null ? "log.txt" : $"{assembly_filename}_log.txt";
+        private static string Assembly_filename => $"{Assembly.GetExecutingAssembly().GetName().Name}";
+        private static string Filename => Assembly_filename == null ? "log.txt" : $"{Assembly_filename}_log.txt";
 
         public static void ClearLogFile()
         {
-            File.WriteAllText(filename, string.Empty);
+            File.WriteAllText(Filename, string.Empty);
         }
 
         public static void Log(string text)
         {
             var line = $"[{DateTime.Now}] {text}";
-            File.AppendAllText(filename, line);
+            File.AppendAllText(Filename, line);
         }
 
         public static void LogLine(string text)
         {
             var line = $"[{DateTime.Now}] {text}\n";
-            File.AppendAllText(filename, line);
+            File.AppendAllText(Filename, line);
         }
     }
 }
