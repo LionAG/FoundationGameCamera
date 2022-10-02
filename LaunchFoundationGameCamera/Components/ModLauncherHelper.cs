82b07e2964df61d8f13a7033386c7c32069ccc82
Nesae <92125893+Nesae-avi@users.noreply.github.com>
2022-10-02 20:51:31 +0200

    Fix DirectX check

--- LaunchFoundationGameCamera/Components/ModLauncherHelper.cs
+++ LaunchFoundationGameCamera/Components/ModLauncherHelper.cs
@@ -12,11 +12,13 @@ namespace LaunchFoundationGameCamera.Components
         public static bool ROTTR_IsDirectX11()
         {
             var graphicsKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Crystal Dynamics\Rise of the Tomb Raider\Graphics");
-
-            if(graphicsKey != null && graphicsKey.GetValue("EnableDX12") is bool enableDX12)
+            
+            if(graphicsKey != null && graphicsKey.GetValue("EnableDX12") is object enableDX12)
             {
+                graphicsKey.Close();
+
                 // Return true when DirectX 12 is disabled
-                return !enableDX12;
+                return !Convert.ToBoolean(enableDX12);
             }
 
             return false;
@@ -26,10 +28,12 @@ namespace LaunchFoundationGameCamera.Components
         {
             var graphicsKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Eidos Montreal\Shadow of the Tomb Raider\Graphics");
 
-            if (graphicsKey != null && graphicsKey.GetValue("EnableDX12") is bool enableDX12)
+            if (graphicsKey != null && graphicsKey.GetValue("EnableDX12") is object enableDX12)
             {
+                graphicsKey.Close();
+
                 // Return true when DirectX 12 is disabled
-                return !enableDX12;
+                return !Convert.ToBoolean(enableDX12);
             }
 
             return false;
