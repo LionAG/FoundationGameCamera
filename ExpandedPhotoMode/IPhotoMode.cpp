38f1e10773cd49f5c45ce73e7d0ea7f6f1c2d488
Nesae <92125893+Nesae-avi@users.noreply.github.com>
2022-10-02 20:59:49 +0200

    Update offsets

--- ExpandedPhotoMode/IPhotoMode.cpp
+++ ExpandedPhotoMode/IPhotoMode.cpp
@@ -8,9 +8,9 @@ void Nesae::ExpandedPhotoMode::IPhotoMode::EnableOverride()
     auto data = new byte[6];
     memset(data, 0x90, 6);
 
-    auto coordinateX = (QWORD)GetModuleHandle(NULL) + 0x8CB855;
-    auto coordinateY = (QWORD)GetModuleHandle(NULL) + 0x8CB85B;
-    auto coordinateZ = (QWORD)GetModuleHandle(NULL) + 0x8CB895;
+    auto coordinateX = (QWORD)GetModuleHandle(NULL) + 0x8CB835;
+    auto coordinateY = (QWORD)GetModuleHandle(NULL) + 0x8CB83B;
+    auto coordinateZ = (QWORD)GetModuleHandle(NULL) + 0x8CB875;
 
     DWORD oldProtect, sOldProtect;
     VirtualProtect((LPVOID)coordinateX, 0x1000, PAGE_READWRITE, &oldProtect);
@@ -29,9 +29,9 @@ void Nesae::ExpandedPhotoMode::IPhotoMode::EnableOverride()
 
 void Nesae::ExpandedPhotoMode::IPhotoMode::DisableOverride()
 {
-    auto coordinateX = (QWORD)GetModuleHandle(NULL) + 0x8CB855;
-    auto coordinateY = (QWORD)GetModuleHandle(NULL) + 0x8CB85B;
-    auto coordinateZ = (QWORD)GetModuleHandle(NULL) + 0x8CB895;
+    auto coordinateX = (QWORD)GetModuleHandle(NULL) + 0x8CB835;
+    auto coordinateY = (QWORD)GetModuleHandle(NULL) + 0x8CB83B;
+    auto coordinateZ = (QWORD)GetModuleHandle(NULL) + 0x8CB875;
 
     byte dataX[6] = { 0xF3, 0x0F, 0x11, 0x44, 0x24, 0x30 };
     byte dataY[6] = { 0xF3, 0x0F, 0x11, 0x4C, 0x24, 0x34 };
