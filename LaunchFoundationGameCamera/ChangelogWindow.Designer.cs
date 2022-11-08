c3e7a5b8770158ec08503f75cf6ed43857264386
Nesae <92125893+Nesae-avi@users.noreply.github.com>
2022-11-08 01:07:18 +0100

    Update window icon

--- LaunchFoundationGameCamera/ChangelogWindow.Designer.cs
+++ LaunchFoundationGameCamera/ChangelogWindow.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChangelogWindow));
             this.richTextBox_Changelog = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
@@ -53,6 +54,7 @@
             this.ClientSize = new System.Drawing.Size(784, 401);
             this.Controls.Add(this.richTextBox_Changelog);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "ChangelogWindow";
             this.Text = "Changelog";
